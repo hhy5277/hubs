abbe54d011de31d73d98965c6f72c7672e50a400
Marshall Polaris <marshall@pol.rs>
2019-03-14 17:43:16 -0700

    Remove proxying for the 'loading' scene

--- src/hub.js
+++ src/hub.js
@@ -174,8 +174,7 @@ import qsTruthy from "./utils/qs_truthy";
 const isBotMode = qsTruthy("bot");
 const isTelemetryDisabled = qsTruthy("disable_telemetry");
 const isDebug = qsTruthy("debug");
-const loadingEnvironmentURL =
-  "https://hubs-proxy.com/https://uploads-prod.reticulum.io/files/61d77151-7a74-40a6-b427-0c5a350c4502.glb";
+const loadingEnvironmentURL = "https://uploads-prod.reticulum.io/files/61d77151-7a74-40a6-b427-0c5a350c4502.glb";
 
 if (!isBotMode && !isTelemetryDisabled) {
   registerTelemetry("/hub", "Room Landing Page");
